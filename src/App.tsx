5f3e993bfa6a4f0f9b61882b455c37b7e57e48ee
dev <webstar0103@gmail.com>
2025-05-02 01:43:44 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+export default function App() {
+  return (
+    <div className="flex items-center justify-center w-full h-screen">
+      <p className="text-2xl font-bold bg-primary">Hello World</p>
+    </div>
+  );
+}
