17e3d79dcdb2fcf8f9235c01ad0a9065cfad8439
dev <webstar0103@gmail.com>
2025-05-03 11:31:43 +0300

    update: vercel issues

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import vercel from 'vite-plugin-vercel';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vercel()],
+  server: {
+    port: 5173,
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
